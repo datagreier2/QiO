fce6294e346310d6fdc7e517c2459859089938fa
datagreier2 <217688407+datagreier2@users.noreply.github.com>
2025-10-21 12:26:09 +0200

    Fixed another build slug issue

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/qio/',
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/QiO/' : '/',
   plugins: [react()],
-})
+}))
